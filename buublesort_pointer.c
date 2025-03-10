aae8677b057bcbad0dc645e2659c052c851fee21
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-09 20:29:58 -1000

    Update and rename sum_of_rows_n_cols.c to buublesort_pointer.c

---
+++ buublesort_pointer.c
@@ -0,0 +1,12 @@
+/*
+Write a C program that sorts an array of integers using the Bubble Sort algorithm, with pointers.
+
+The program should:
+• Allow the user to input an array of integers.
+• Sort the array in ascending order using the Bubble Sort algorithm.
+• Display the sorted array after sorting.
+
+** See PDF for sample input/output
+*/
+
+
